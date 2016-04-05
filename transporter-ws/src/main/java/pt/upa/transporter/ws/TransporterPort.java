10932e9c99cfa34b1030ed2b995d062042caa2bd
henrical <henrique.cal94@gmail.com>
2016-04-05 14:29:22 +0100

    implementaçao da classe TransporterPort que vinha vazia no projecto

--- transporter-ws/src/main/java/pt/upa/transporter/ws/TransporterPort.java
+++ transporter-ws/src/main/java/pt/upa/transporter/ws/TransporterPort.java
@@ -1,7 +1,42 @@
 package pt.upa.transporter.ws;
 
-public class TransporterPort {
+import javax.jws.WebService;
+
+import java.util.List;
+
+@WebService(endpointInterface = "pt.upa.transporter.ws.TransporterPortType")
+public class TransporterPort implements TransporterPortType {
 
 	// TODO
 
+	public void clearJobs(){
+	
+	}
+	
+	public List<JobView> listJobs(){
+	
+            return null;
+	}
+	
+	public JobView jobStatus(String id){
+
+            return null;
+	}
+	
+	public JobView decideJob(String id,boolean accept) throws BadJobFault_Exception
+	{
+            return null;
+	}
+	
+	public JobView requestJob(String origin, String destination, int price)
+        throws BadLocationFault_Exception, BadPriceFault_Exception
+	{
+            return null;
+	}
+	
+        public String ping(String name){
+        
+            return null;
+        }
+	
 }
