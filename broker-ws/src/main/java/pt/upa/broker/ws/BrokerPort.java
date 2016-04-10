5aafad0f27afcbcf05bd5f15553f72daa1d2e0c2
FonsecaGitHub <miguel.fonseca1011011@gmail.com>
2016-04-10 18:10:04 +0100

    broker-ws pom; BrokerPort class implemented

--- broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
+++ broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
@@ -1,7 +1,31 @@
 package pt.upa.broker.ws;
 
-public class BrokerPort {
+import javax.jws.WebService;
 
-	// TODO
+import java.util.List;
 
+@WebService(endpointInterface = "pt-upa.broker.ws.BrokerPortType")
+public class BrokerPort implements BrokerPortType {
+
+	public void clearTransports() {
+
+	}
+
+	public List<TransportView> listTransports() {
+		return null;
+	}
+
+	public TransportView viewTransport(String id)
+		throws UnknownTransportFault_Exception {
+			return null;
+	}
+
+	public String requestTransport(String origin, String destination, int price)
+		throws InvalidPriceFault_Exception, UnavailableTransportFault_Exception, UnavailableTransportPriceFault_Exception, UnknownLocationFault_Exception {
+			return null;
+	}
+
+	public String ping(String name) {
+		return null;
+	}
 }
