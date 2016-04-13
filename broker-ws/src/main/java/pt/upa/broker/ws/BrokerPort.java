a2d63df3b0f2efd4862bcb60e0625a415a6b6ed7
henrical <henrique.cal94@gmail.com>
2016-04-14 00:45:37 +0100

    requestJob no trans-ws

--- broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
+++ broker-ws/src/main/java/pt/upa/broker/ws/BrokerPort.java
@@ -2,16 +2,11 @@ package pt.upa.broker.ws;
 
 import javax.jws.WebService;
 
-import org.apache.commons.lang.ArrayUtils;
+//pt.upa.transporter.ws
+import pt.upa.transporter.ws.cli.TransporterClient;
 
 import java.util.List;
 import java.util.LinkedList;
-import java.util.ArrayList;
-
-
-import pt.upa.transporter.ws.JobView;
-//pt.upa.transporter.ws
-import pt.upa.transporter.ws.cli.TransporterClient;
 
 @WebService(endpointInterface = "pt.upa.broker.ws.BrokerPortType")
 public class BrokerPort implements BrokerPortType {
@@ -41,11 +36,9 @@ public class BrokerPort implements BrokerPortType {
         
         private List<TransporterClient> _transporters;
         
-        private LinkedList<TransportView> _transports;
-        
         public BrokerPort()
         {
-            _transporters = new LinkedList<TransporterClient>();              
+            _transporters = new LinkedList<TransporterClient>();
         }
         
         //======= Metodos genericos. ==================================================
@@ -92,51 +85,21 @@ public class BrokerPort implements BrokerPortType {
 	}
 
 	public List<TransportView> listTransports() {
-		
+		return null;
 	}
 
-  // Devolve o estado actual do Transport id
 	public TransportView viewTransport(String id)
 		throws UnknownTransportFault_Exception {
 			return null;
 	}
 
-  // Tenta marcar um transporte de uma origem para um destido, com um preco maximo maior que 0
 	public String requestTransport(String origin, String destination, int price)
 		throws InvalidPriceFault_Exception, UnavailableTransportFault_Exception, UnavailableTransportPriceFault_Exception, UnknownLocationFault_Exception {
-
-		if(price < 0) {
-			//TODO throw new InvalidPriceFault_Exception(price);
-		}
-		
-		if(!ArrayUtils.contains(LOCATIONS_NORTH_REGION, origin) &&
-		   !ArrayUtils.contains(LOCATIONS_CENTER_REGION, origin) &&
-		   !ArrayUtils.contains(LOCATIONS_SOUTH_REGION, origin)) {
-			//TODO throw new UnknownLocationFault_Exception();
-		}
-		
-		if(!ArrayUtils.contains(LOCATIONS_NORTH_REGION, destination) &&
-		   !ArrayUtils.contains(LOCATIONS_CENTER_REGION, destination) &&
-		   !ArrayUtils.contains(LOCATIONS_SOUTH_REGION, destination)) {
-			//TODO throw new UnknownLocationFault_Exception();
-		}
-		
-		for(TransportView transport : _transports) {
-			if(transport.getOrigin().equals(origin) && transport.getDestination().equals(destination)) {
-				if(transport.getPrice() <= price) {
-					//TODO Adiciona o servico?
-				} else {
-					throw new UnavailableTransportPriceFault_Exception(destination, null); 
-				}
-			} else { 	
-				throw new UnavailableTransportFault_Exception(destination, null);
-			}
-		}
-		return "Transport Requested - " + "Origin: " + origin + "; Destination" + destination + "; Price: " + price;
-	}
 			
 		
-	
+		
+                return null;
+	}
 
 	public String ping(String name) {
 		return "<<< Broker Pinged by \"" + name + "\"! >>>";	
